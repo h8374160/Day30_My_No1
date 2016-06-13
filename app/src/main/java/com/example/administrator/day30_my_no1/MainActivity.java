bb053e49cb50c374c67d7ce854a858458c2ceb7d
h8374160 <ouxingtian@163.com>
2016-06-13 19:09:15 +0800

    刘杰

--- app/src/main/java/com/example/administrator/day30_my_no1/MainActivity.java
+++ app/src/main/java/com/example/administrator/day30_my_no1/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //刘杰大傻逼
+        //哈哈强大傻逼
     }
 }
